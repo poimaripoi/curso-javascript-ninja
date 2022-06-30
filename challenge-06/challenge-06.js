e8689a897a6fcff0a0f5723eab092719546b6873
Marina Takahashi <88720221+poimaripoi@users.noreply.github.com>
2022-06-30 17:10:26 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'libertadores'
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,8 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
-
+var teams = [ 'Flamengo' , 'Palmeiras' , 'Atlético' , 'Grêmio' , 'Santos']
 console.log( 'Times que estão participando do campeonato:', teams );
 
 /*
@@ -32,19 +32,51 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(posicao){
+    var sub = posicao;
+    var position = --sub;
+    if(posicao >=1 && posicao <= 5)
+    console.log("O time que está em " + posicao + "º lugar é o " + teams[position] + ".")
+    else {
+        return "Não temos a informação do time que está nessa posição."
+    }
+}
+
+/* resposta do professor, mais limpa
+function showTeamPosition (position) {
+  if (position < 1 || position > 5) {
+    return 'Não temos a informação do time que está nessa posição.';
+  }
+
+  return 'O time que está em ' + position + 'º lugar é o ' + teams[position - 1] + '.';
+} 
+*/
 
 /*
-Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
-função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
+Escolha 4 times do campeonato selecionado e mostre a posição dele, usando afunção acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+showTeamPosition(1) // O time que está em 1º lugar é o Flamengo.
+showTeamPosition(2) // O time que está em 1º lugar é o Palmeiras.
+showTeamPosition(3) // O time que está em 1º lugar é o Atlético.
+showTeamPosition(-1) // Não temos a informação do time que está nessa posição.
+
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+while(counter <= 30){
+    console.log(counter);
+    counter++;
+}
+
+/* resposta do professor simplificado
+var num = 20;
+while (num <= 30) {
+  console.log(num++);
+}
+*/
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +90,39 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(cor){
+    var hex;
+    switch (cor){
+        case 'vermelho':
+            hex =  '#FF0000'
+            break;
+        case 'azul':
+            hex = '#0000FF'
+            break;
+        case 'verde':
+            hex = '#008000	'
+            break;
+        case 'amarelo':
+            hex = '#FFFF00'
+            break;
+        case 'laranja':
+            hex = '#FFA500'
+            break;
+        default:
+            return"Não temos o equivalente hexadecimal para "+ cor;
+    }
+        return "O hexadecimal para a cor " + cor + " é " + hex;
+}
+//porque aqui com console.log o retorno da erro?
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+convertToHex('vermelho') // O hexadecimal para a cor vermelho é #FF0000
+convertToHex('azul') // O hexadecimal para a cor vermelho é #0000FF
+convertToHex('verde') // O hexadecimal para a cor vermelho é #008000
+convertToHex('amarelo') // O hexadecimal para a cor vermelho é #FFFF00
+convertToHex('laranja') // O hexadecimal para a cor vermelho é #FFA500
+convertToHex('rosa') // Não temos o equivalente hexadecimal para rosa
+convertToHex('marrom') // Não temos o equivalente hexadecimal para marrom
+convertToHex('roxo') // Não temos o equivalente hexadecimal para roxo
