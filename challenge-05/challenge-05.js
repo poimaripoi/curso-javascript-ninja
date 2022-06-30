ad5b5f2752e739194114851c28cf66bb02aaf612
Marina Takahashi <88720221+poimaripoi@users.noreply.github.com>
2022-06-30 17:09:38 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -1,76 +1,183 @@
+# Desafio da semana #4
+
+```js
 /*
-Crie uma variável qualquer, que receba um array com alguns valores aleatórios
-- ao menos 5 - (fica por sua conta os valores do array).
+Declare uma variável chamada `isTruthy`, e atribua a ela uma função que recebe
+um único parâmetro como argumento. Essa função deve retornar `true` se o
+equivalente booleano para o valor passado no argumento for `true`, ou `false`
+para o contrário.
 */
-// ?
+// var isTruthy = function(x){
+//     return x ? true : false;
+
+var isTruthy = function(a){
+    return !!a;
+}
+
+// Invoque a função criada acima, passando todos os tipos de valores `falsy`.
+isTruthy("")
+// false
+isTruthy(false)
+// false
+isTruthy(0)
+// false
+isTruthy()
+// false
+isTruthy(-0)
+// false
+isTruthy(undefined)
+// false
+isTruthy(NaN)
+// false
+isTruthy(null)
+// false
+
 
 /*
-Crie uma função que receba um array como parâmetro, e retorne esse array.
+Invoque a função criada acima passando como parâmetro 10 valores `truthy`.
 */
-// ?
+isTruthy('mari')
+// true
+isTruthy(1,2,3)
+// true
+isTruthy([1,2,3])
+// true
+isTruthy(1 * 9)
+// true
+
 
 /*
-Imprima o segundo índice do array retornado pela função criada acima.
+Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
+seguintes propriedades (os valores devem ser do tipo mostrado abaixo):
+- `marca` - String
+- `modelo` - String
+- `placa` - String
+- `ano` - Number
+- `cor` - String
+- `quantasPortas` - Number
+- `assentos` - Number - cinco por padrão
+- `quantidadePessoas` - Number - zero por padrão
 */
-// ?
+var carro = { marca: "sandero" , modelo: "naoseikk" , placa: "0000000" , ano: 2005 , cor: "azul" , quantasPortas: 4 , assentos: 5 , quantidadePessoas: 0}
 
 /*
-Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
-segundo, um número. A função deve retornar o valor de um índice do array que foi passado
-no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
-segundo parâmetro.
+Crie um método chamado `mudarCor` que mude a cor do carro conforme a cor  
+passado por parâmetro.
 */
-// ?
+carro.mudaCor = function(cor){
+    carro.cor = cor;
+}
 
 /*
-Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
+Crie um método chamado `obterCor`, que retorne a cor do carro.
 */
-// ?
+carro.obterCor = function(){
+    return carro.cor;
+}
 
 /*
-Invoque a função criada acima, fazendo-a retornar todos os valores do último
-array criado.
+Crie um método chamado `obterModelo` que retorne o modelo do carro.
 */
-// ?
+carro.obterModelo = function(){
+    return carro.modelo;
+}
 
 /*
-Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
-livro. Dentro dessa função, declare uma variável que recebe um objeto com as
-seguintes características:
-- esse objeto irá receber 3 propriedades, que serão nomes de livros;
-- cada uma dessas propriedades será um novo objeto, que terá outras 3
-propriedades:
-    - `quantidadePaginas` - Number (quantidade de páginas)
-    - `autor` - String
-    - `editora` - String
-- A função deve retornar o objeto referente ao livro passado por parâmetro.
-- Se o parâmetro não for passado, a função deve retornar o objeto com todos
-os livros.
+Crie um método chamado `obterMarca` que retorne a marca do carro.
 */
-// ?
+carro.obterMarca = function(){
+    return carro.marca;
+}
 
 /*
-Usando a função criada acima, imprima o objeto com todos os livros.
+Crie um método chamado `obterMarcaModelo`, que retorne:
+"Esse carro é um [MARCA] [MODELO]"
+Para retornar os valores de marca e modelo, utilize os métodos criados.
 */
-// ?
+carro.obterMarcaModelo = function(){
+    return "Esse carro é um " + carro.obterMarca() + " " + carro.obterModelo();
+}
 
-/*
-Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
-usando a frase:
-"O livro [NOME_DO_LIVRO] tem [X] páginas!"
-*/
-// ?
 
-/*
-Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
-a frase:
-"O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
+/*Crie um método que irá adicionar pessoas no carro. Esse método terá asseguintes características:
+- Ele deverá receber por parâmetro o número de pessoas entrarão no carro. Esse número não precisa encher o carro, você poderá acrescentar as pessoas aos poucos.
+- O método deve retornar a frase: "Já temos [X] pessoas no carro!"
+- Se o carro já estiver cheio, com todos os assentos já preenchidos, o método
+deve retornar a frase: "O carro já está lotado!"
+- Se ainda houverem lugares no carro, mas a quantidade de pessoas passadas por
+parâmetro for ultrapassar o limite de assentos do carro, então você deve
+mostrar quantos assentos ainda podem ser ocupados, com a frase:
+"Só cabem mais [QUANTIDADE_DE_PESSOAS_QUE_CABEM] pessoas!"
+- Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
+citado acima, no lugar de "pessoas".
 */
-// ?
+carro.adcPessoa = function(pessoas){
+    var totalPessoas = pessoas + carro.quantidadePessoas;
 
+    if(carro.quantidadePessoas === carro.assentos && totalPessoas >= carro.assentos){
+        return "O carro já está lotado!"  
+    }
+    
+    if(totalPessoas > carro.assentos){
+        var pessoasCabem = carro.assentos - carro.quantidadePessoas;
+        var quantidade = pessoasCabem === 1 ? ' pessoa' : ' pessoas';
+        return 'Só cabem mais ' + pessoasCabem + quantidade + '!';
+    }
+
+    carro.quantidadePessoas += pessoas;
+
+    return 'Já temos ' + carro.quantidadePessoas + ' pessoas no carro!';
+}
 /*
-Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
-a frase:
-"O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
+Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
+utilize sempre o formato de invocação do método (ou chamada da propriedade),
+adicionando comentários _inline_ ao lado com o valor retornado, se o método
+retornar algum valor.
+
+Qual a cor atual do carro?
 */
-// ?
+carro.obterCor()
+'verde'
+
+// Mude a cor do carro para vermelho.
+carro.mudaCor('vermelho')
+
+// E agora, qual a cor do carro?
+carro.obterCor()
+'vermelho'
+
+// Mude a cor do carro para verde musgo.
+carro.mudaCor('verde musgo')
+
+// E agora, qual a cor do carro?
+carro.obterCor()
+'verde musgo'
+
+// Qual a marca e modelo do carro?
+carro.obterMarcaModelo()
+'Esse carro é um sandero naoseikk'
+
+// Adicione 2 pessoas no carro.
+carro.adcPessoa(2)
+'Já temos 2 pessoas no carro!'
+
+// Adicione mais 4 pessoas no carro.
+carro.adcPessoa(4)
+'Só cabem mais 3 pessoas!'
+
+// Faça o carro encher.
+carro.adcPessoa(3)
+'Já temos 5 pessoas no carro!'
+
+// Tire 4 pessoas do carro.
+carro.adcPessoa(-4)
+'Já temos 1 pessoas no carro!'
+
+// Adicione 10 pessoas no carro.
+carro.adcPessoa(10)
+'Só cabem mais 4 pessoas!'
+
+// Quantas pessoas temos no carro?
+carro.quantidadePessoas;
+1
+```
